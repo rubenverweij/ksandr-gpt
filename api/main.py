00b79df2d17af13cbd590f43d93a2978944abccb
RubenV <ruben.verweij@datapreds.com>
2025-06-24 16:04:09 +0200

    Updated requests

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@
 TAALMODEL API KSANDR
 """
 
+from typing import List, Optional
+from pydantic import BaseModel
 from onprem import LLM
 from fastapi import FastAPI
 
@@ -10,39 +12,57 @@ app = FastAPI()
 llm = LLM(n_gpu_layers=-1)
 
 
+class AskRequest(BaseModel):
+    prompt: str
+    permission: Optional[List[int]] = None
+    aad: Optional[List[int]] = None
+
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
 
 @app.post("/ask")
-def ask_question(prompt: str, permission: list[int] | None, aad: list[int] | None):
-    """Perform a query with optional permission and aad filters.
+def ask(request: AskRequest):
+    """Perform a query with optional permission and aad filters."""
+    permission = request.permission
+    aad = request.aad
 
-    Args:
-        prompt (str): The users prompt.
-        permission (list[int] | None): List of permission ids.
-        aad (list[int] | None): List of aad ids.
-
-    Returns:
-        dict | None: The filter object, or None if no filters.
-    """
     filters = []
     if permission:
         filters.append({"permission": {"$in": permission}})
+
     if aad:
         filters.append({"aad": {"$in": aad}})
 
     if not filters:
-        filter_obj = {"table": False}
+        filter_obj = {"table": True}
     elif len(filters) == 1:
         filter_obj = filters[0]
     else:
-        # Both present
         filter_obj = {"$and": filters}
-    return llm._ask(prompt, filters=filter_obj, table_k=0)
+    return llm._ask(prompt=request.prompt, filters=filter_obj, table_k=0)
 
 
 @app.post("/chat")
-def chat_with_llm(prompt: str):
-    return llm.chat(prompt)
+def chat(request: AskRequest):
+    """Perform a query with optional permission and aad filters."""
+    permission = request.permission
+    aad = request.aad
+
+    filters = []
+    if permission:
+        filters.append({"permission": {"$in": permission}})
+
+    if aad:
+        filters.append({"aad": {"$in": aad}})
+
+    if not filters:
+        filter_obj = {"table": True}
+    elif len(filters) == 1:
+        filter_obj = filters[0]
+    else:
+        filter_obj = {"$and": filters}
+
+    return llm.chat(prompt=request.prompt, filters=filter_obj, table_k=0)
