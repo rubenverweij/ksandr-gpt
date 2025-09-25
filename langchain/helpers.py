0e3b939407c69ffe59d539cfcb023b32b6461ed3
RubenV <ruben.verweij@datapreds.com>
2025-09-25 16:08:43 +0200

    Add new embedding model

--- langchain/helpers.py
+++ langchain/helpers.py
@@ -61,11 +61,11 @@ def uniek_antwoord(tekst):
 
 
 def get_embedding_function():
-    embedding_encode_kwargs: dict = {"normalize_embeddings": False}
+    embedding_encode_kwargs: dict = {"normalize_embeddings": True}
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    embedding_model_kwargs = {"device": device}
+    embedding_model_kwargs = {"device": device, "torch_dtype": torch.float16}
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L12-v2",
+        model_name="BAAI/bge-multilingual-gemma2",
         model_kwargs=embedding_model_kwargs,
         encode_kwargs=embedding_encode_kwargs,
     )
