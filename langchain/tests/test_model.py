295adfa665d6977bdd3a6eafab9e820364d3d8e4
RubenV <ruben.verweij@datapreds.com>
2025-09-26 15:53:34 +0200

    Added test structure

--- langchain/tests/test_model.py
+++ langchain/tests/test_model.py
@@ -1,11 +1,10 @@
 import csv
-import json
 import requests
 import argparse
 from datetime import datetime
-import re
 
-url = "http://localhost:8080/evaluate"
+url_question = "http://localhost:8080/ask"
+url_evaluate = "http://localhost:8080/evaluate"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -17,8 +16,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="/home/ubuntu/onprem_data/tests/",
+        default="/home/ubuntu/onprem_data/tests/reports",
         help="Path to output dir",
+    )
+    parser.add_argument(
+        "--question_col",
+        type=str,
+        default="Vraag",
+        help="Column name for questions",
     )
     parser.add_argument(
         "--expected_col",
@@ -29,7 +34,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--actual_col",
         type=str,
-        default="Resultaat_19-9-2025",
+        default="Resultaat",
         help="Column name for actual answers",
     )
     args = parser.parse_args()
@@ -52,47 +57,55 @@ if __name__ == "__main__":
             percent = (idx / total) * 100
             print(f"Processing test question {idx}/{total} ({percent:.1f}%)")
 
+            question = row[args.question]
             expected = row[args.expected_col]
             actual = row[args.actual_col]
-            payload = {"expected": expected, "actual": actual}
 
-            response = requests.post(url, json=payload)
-            data = response.json()
-            response_text = data["evaluation"].strip().lower()
+            payload = {"prompt": question}
+            response = requests.post(url_question, json=payload)
+            print(response)
+            if idx == 1:
+                break
 
-            match = re.search(
-                r"\b([0-9](?:\.\d+)?|10(?:\.0+)?)(?:/10)?\b", response_text
-            )
-            if match:
-                score = float(match.group(1))
-            else:
-                score = None
+    #         payload = {"expected": expected, "actual": actual}
 
-            if response_text.startswith("correct"):
-                result = "correct"
-            elif response_text.startswith("incorrect"):
-                result = "incorrect"
-            else:
-                result = "unknown"
+    #         response = requests.post(url, json=payload)
+    #         data = response.json()
+    #         response_text = data["evaluation"].strip().lower()
 
-            results.append(
-                {
-                    "actual": actual,
-                    "expected": expected,
-                    "resultaat": result,
-                    "toelichting": response_text,
-                    "score": score,
-                }
-            )
+    #         match = re.search(
+    #             r"\b([0-9](?:\.\d+)?|10(?:\.0+)?)(?:/10)?\b", response_text
+    #         )
+    #         if match:
+    #             score = float(match.group(1))
+    #         else:
+    #             score = None
 
-    # Schrijf naar JSON bestand
-    with open(output_file, "w", encoding="utf-8") as f:
-        json.dump(results, f, ensure_ascii=False, indent=2)
+    #         if response_text.startswith("correct"):
+    #             result = "correct"
+    #         elif response_text.startswith("incorrect"):
+    #             result = "incorrect"
+    #         else:
+    #             result = "unknown"
 
-    total = len(results)
-    num_correct = sum(1 for r in results if r["resultaat"] == "correct")
+    #         results.append(
+    #             {
+    #                 "actual": actual,
+    #                 "expected": expected,
+    #                 "resultaat": result,
+    #                 "toelichting": response_text,
+    #                 "score": score,
+    #             }
+    #         )
 
-    print(f"✅ Results saved to {output_file}")
-    print(f"Totaal aantal vragen: {total}")
-    print(f"Aantal correct: {num_correct}")
-    print(f"Aantal incorrect: {total - num_correct}")
+    # # Schrijf naar JSON bestand
+    # with open(output_file, "w", encoding="utf-8") as f:
+    #     json.dump(results, f, ensure_ascii=False, indent=2)
+
+    # total = len(results)
+    # num_correct = sum(1 for r in results if r["resultaat"] == "correct")
+
+    # print(f"✅ Results saved to {output_file}")
+    # print(f"Totaal aantal vragen: {total}")
+    # print(f"Aantal correct: {num_correct}")
+    # print(f"Aantal incorrect: {total - num_correct}")
