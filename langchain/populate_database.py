ff8ec9b1ba8e95e2f2250ee578606b877876e007
RubenV <ruben.verweij@datapreds.com>
2025-10-29 12:43:37 +0100

    Updated schema

--- langchain/populate_database.py
+++ langchain/populate_database.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Dict, List, Union
 
 from bs4 import BeautifulSoup
-from langchain.schema.document import Document
+from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveJsonSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
