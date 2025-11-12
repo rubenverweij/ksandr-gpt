31be48b9cbb6006eebc738fe922f3ed488865917
RubenV <ruben.verweij@datapreds.com>
2025-11-12 08:46:50 +0100

    Added from langchain_neo4j import Neo4jGraph

--- langchain/main.py
+++ langchain/main.py
@@ -23,7 +23,7 @@ from langchain_chroma import Chroma
 from langchain_community.llms import LlamaCpp
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_neo4j import Neo4jGraph
-from langchain_community.chains.graph_qa.cypher import GraphCypherQAChain
+from langchain.chains.graph_qa.cypher import GraphCypherQAChain
 from langchain_core.output_parsers import BaseOutputParser
 from langchain_core.prompts import PromptTemplate
 
