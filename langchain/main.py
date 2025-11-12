77ffd9808ccb6b85e8b665dfe12687a4fc75bb0e
RubenV <ruben.verweij@datapreds.com>
2025-11-12 11:00:42 +0100

    Added schema

--- langchain/main.py
+++ langchain/main.py
@@ -426,9 +426,7 @@ Cypher:
 def neo(req: Neo4jRequest):
     question = req.prompt
     schema = graph.schema  # Optional, or pass a string manually
-    cypher_text = LLM.invoke(
-        cypher_prompt.format(schema=schema, question=question)
-    ).content
+    cypher_text = LLM.invoke(cypher_prompt.format(schema=schema, question=question))
     clean_cypher = CypherOutputParser().parse(cypher_text)
     logger.info("✅ Clean Cypher used:\n%s", clean_cypher)
     try:
