374ff7bd72628a4aef5e3b78d01dbc31bc8f6b81
RubenV <ruben.verweij@datapreds.com>
2025-11-24 16:03:58 +0100

    Added embeddings

---
+++ langchain/graphdb/embeddings.py
@@ -0,0 +1,14 @@
+import torch
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def get_embedding_function():
+    embedding_encode_kwargs: dict = {"normalize_embeddings": True}
+    device = torch.device("cuda")
+    embedding_model_kwargs = {"device": device}
+    embeddings = HuggingFaceEmbeddings(
+        model_name="NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers",
+        model_kwargs=embedding_model_kwargs,
+        encode_kwargs=embedding_encode_kwargs,
+    )
+    return embeddings
