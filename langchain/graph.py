947d4207588943ef82151e5ab76066bd09d99f15
RubenV <ruben.verweij@datapreds.com>
2026-01-12 11:29:21 +0100

    Inluded return placeholder

--- langchain/graph.py
+++ langchain/graph.py
@@ -15,9 +15,9 @@ BASE_QUERY_SPEC = """
 WITH $aad_ids AS dossier_ids, $permissions AS permissions
 UNWIND keys(permissions) AS category
 UNWIND permissions[category] AS allowed_dossier_id
-MATCH (d:dossier {aad_id: allowed_dossier_id})-[:HEEFT_COMPONENT]->(c:component)-[:HEEFT_FAALVORM]->(f:faalvorm)
-MATCH (d)-[:HAS_PERMISSION]->(:permission {category: category})
-MATCH (f)-[:HAS_PERMISSION]->(:permission {category: category})
+MATCH (d:dossier {{aad_id: allowed_dossier_id}})-[:HEEFT_COMPONENT]->(c:component)-[:HEEFT_FAALVORM]->(f:faalvorm)
+MATCH (d)-[:HAS_PERMISSION]->(:permission {{category: category}})
+MATCH (f)-[:HAS_PERMISSION]->(:permission {{category: category}})
 WHERE size(dossier_ids) = 0 OR d.aad_id IN dossier_ids
 {return_clause}
 """
