97df490600d4004be43c1950ae715396c3ac073b
RubenV <ruben.verweij@datapreds.com>
2026-02-04 10:42:02 +0100

    CPU embedding

--- langchain/ingest/embeddings.py
+++ langchain/ingest/embeddings.py
@@ -4,7 +4,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_embedding_function():
     embedding_encode_kwargs: dict = {"normalize_embeddings": True}
-    device = torch.device("cuda")
+    device = torch.device("cpu")
     embedding_model_kwargs = {"device": device}
     embeddings = HuggingFaceEmbeddings(
         model_name="NetherlandsForensicInstitute/robbert-2022-dutch-sentence-transformers",
