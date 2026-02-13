7705ee998cb7519ba54ac771b3c03f146a090c72
RubenV <ruben.verweij@datapreds.com>
2026-02-13 12:03:34 +0100

    Fixed path

--- ksandr/settings/config.py
+++ ksandr/settings/config.py
@@ -150,7 +150,7 @@ WEBLOCATION_TEMPLATE = [
 ]
 
 try:
-    with open("ksandr/creds.json") as f:
+    with open("/ksandr-gpt/ksandr/creds.json") as f:
         SECRETS = json.load(f)
 except FileNotFoundError:
     with open(f"{LOCAL_DIR}/config/creds.json") as f:
