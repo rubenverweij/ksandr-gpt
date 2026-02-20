a1924dd2eca42d608130c5cfd24a233a16fc6e70
RubenV <ruben.verweij@datapreds.com>
2026-02-20 15:51:07 +0100

    Included all worklflows

--- workflow.sh
+++ workflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# python3 -m ksandr.vectorstore.populate_database -env ${APP_ENV}
-# python3 -m ksandr.vectorstore.remove_duplicates -env ${APP_ENV}
-# python3 -m ksandr.graphdb.ingest_cypher_queries -env ${APP_ENV}
+python3 -m ksandr.vectorstore.populate_database -env ${APP_ENV}
+python3 -m ksandr.vectorstore.remove_duplicates -env ${APP_ENV}
+python3 -m ksandr.graphdb.ingest_cypher_queries -env ${APP_ENV}
 python3 -m ksandr.graphdb.ingest_aads -env ${APP_ENV}
